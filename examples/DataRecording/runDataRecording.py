aca9743929f27db9b4c885b78b89cd549d50d3ba
jerrycgc <39204922+jerrycgc@users.noreply.github.com>
2018-05-29 20:40:33 +0800

    合约更新到datarecorder.tickSymbolSet才能录制

--- examples/DataRecording/runDataRecording.py
+++ examples/DataRecording/runDataRecording.py
@@ -53,10 +53,16 @@ def runChildProcess():
         sleep(1)
         contracts = me.getAllContracts()
     print(u"共获取" + str(len(contracts)) + u"条合约信息")
+    tickSymbolSet = set()
     for contract in contracts:
         req = VtSubscribeReq()
         req.symbol = contract.symbol
-        me.subscribe(req, 'CTP')
+        req.exchange = contract.exchange
+        req.productClass = contract.productClass
+        me.subscribe(req, contract.gatewayName)
+        vtSymbol = '.'.join([req.symbol, req.exchange])
+        tickSymbolSet.add(vtSymbol)
+    me.getApp(dataRecorder.appName).tickSymbolSet = tickSymbolSet
 
     while True:
         sleep(1)
